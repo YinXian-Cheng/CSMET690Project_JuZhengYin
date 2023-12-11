35500ac446075dfc6dbf6fd92c66bd15eaa03b5c
Yuan Ju <yuanj@bu.edu>
2023-12-11 02:10:02 -0500

    Updates

--- code/Yuan Ju/ElGamalsteps.py
+++ code/Yuan Ju/ElGamalsteps.py
@@ -14,31 +14,29 @@ class Elgamal:
         self.g = g
         self.x = x
         self.p = p
-        self.y = 0
-        self.Limit_searching_time = 3600
-    def public_key(self):
         self.y = fastExp(self.g,self.x,self.p)
-        return self.y
+        self.Limit_searching_time = 3600
 
-    def encrypt(msg,g,p,y):
-        r = random.randint(2,p-2)
-        c1 = fastExp(g,r,p)
-        c2 = (msg* fastExp(y,r,p)) % p
+    def encrypt(self,msg):
+        r = random.randint(2,self.p-2)
+        print("r",r)
+        c1 = fastExp(self.g,r,self.p)
+        c2 = (msg*fastExp(self.y,r,self.p)) % self.p
         return c1,c2
     
-    def decrypt(x,p,c1,c2):
-        a = fastExp(c1,x,p)
-        msg = (c2 * inverse(a,p)) % p
+    def decrypt(self,c1,c2):
+        a = fastExp(c1,self.x,self.p)
+        msg = (c2 * inverse(a,self.p)) % self.p
         return msg
     
-    def Elgamal_decipher(self,p,g,c1,c2,y):
+    def Elgamal_decipher(self,c1,c2):
         start_time = time.time()
-        x = -1
-        for i in range(1, p):
+        self.x = -1
+        for i in range(1, self.p):
             if time.time() - start_time > self.Limit_searching_time:
                 break
-            if fastExp(g, i, p) == y:
-                x = i
-        a = fastExp(c1,x,p)
-        msg = (c2*inverse(a,p))%p
+            if fastExp(self.g, i, self.p) == self.y:
+                self.x = i
+        a = fastExp(c1,self.x,self.p)
+        msg = (c2*inverse(a,self.p))%self.p
         return msg
